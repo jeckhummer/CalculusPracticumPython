4b20d1c0de739fb74b5b8f055484d6d2d2ef9fcd
Maksim <jeckhummer@gmail.com>
2016-04-07 10:08:32 +0400

    simplified code

--- CalculusPracticum.py
+++ CalculusPracticum.py
@@ -2,30 +2,11 @@ from math import sqrt
 from math import exp
 from math import fabs
 from math import log
+
 import inspect
 
 eps, iterations_limit = 1e-13, 1000
 init_x, alpha = float(input('Enter x: ')), float(input('Enter alpha: '))
-
-
-def write_report(report, show_log = False):
-    str_array = []
-    for ind, phi in enumerate(report):
-        succeed = phi['status'] == 'success'
-        str_array.append(
-                '{status} - phi_{ind}: {phi}{in_case_of_success}'.format(
-                        ind = ind,
-                        phi = inspect.getsource(phi['phi'])[4:-2],
-                        status = phi['status'],
-                        in_case_of_success = ('\n\t{iter} iterations \n\tx = {x} \n\tf(x) = {fx}'
-                                              .format(iter = phi['iterations'],
-                                                      x = phi['x'],
-                                                      fx = phi['fx']) if succeed else ''),
-                )
-        )
-        if succeed and show_log:
-            str_array.extend(phi['log'])
-    return ''.join(map(lambda str: str + '\n', str_array))
 
 
 def init_func(x_):
@@ -39,20 +20,14 @@ phi_pack = [
     lambda x: (alpha + exp(alpha * x)) / x,
 ]
 
-report = [
-    {
-        'status': 'fail',
-        'iterations': 0,
-        'x': None,
-        'fx': None,
-        'log': [],
-        'phi': phi
-    } for ind, phi in enumerate(phi_pack)
-    ]
-
 for ind, phi in enumerate(phi_pack):
-    x, i = init_x, 0
-    dx, func_x = eps * 2, eps * 2  # should be > eps to enter 'while' loop
+    x = init_x
+    i = 0
+    dx = eps * 2  # should be > eps to enter 'while' loop
+    func_x = eps * 2
+    math_error = False
+
+    print(inspect.getsource(phi)[14:-2])
 
     while dx > eps and fabs(func_x) > eps and i < iterations_limit:
         prev_x = x  # save old value of x
@@ -61,22 +36,34 @@ for ind, phi in enumerate(phi_pack):
             x = phi(x)
             func_x = init_func(x)
         except Exception as err:
+            math_error = True
             break
 
         i += 1
         dx = fabs(prev_x - x)
 
         # preparing report
-        report[ind]['log'].append('\tx: {}  dx: {}  func(x): {}'.format(
-        # print('\tx: {}  dx: {}  func(x): {}'.format(
-                str(x).ljust(25),
-                str(dx).ljust(25),
-                str(func_x).ljust(25)))
+        report = '\tx: {}  dx: {}  func(x): {}'.format(
+            str(x).ljust(25),
+            str(dx).ljust(25),
+            str(func_x).ljust(25))
 
-    if i <= iterations_limit and dx < eps and func_x < eps:
-        report[ind]['status'] = 'success'
-        report[ind]['iterations'] = i
-        report[ind]['x'] = x
-        report[ind]['fx'] = func_x
+        # print(report)
 
-print(write_report(report, False))
+    if i == iterations_limit:
+        print('iterations limit reached!')
+    elif math_error:
+        print('math range reached!')
+    elif i < iterations_limit:
+        print('root: {}'.format(x))
+        print('f(x): {}'.format(func_x))
+        print('iterations: {}'.format(i))
+    print('.' * 100)
+
+# if i <= iterations_limit and dx < eps and func_x < eps:
+#     report[ind]['status'] = 'success'
+#     report[ind]['iterations'] = i
+#     report[ind]['x'] = x
+#     report[ind]['fx'] = func_x
+
+# print()
